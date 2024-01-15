33c6d87c4c5e09e8c0f252c18c29014684648ba2
sebastien-dewilde-vinci <sebastien.dewilde@student.vinci.be>
2024-01-15 17:28:03 +0100

    add: feature for verify the gamertag length is at least 8 characters

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -2,8 +2,12 @@
 const { isValid } = require('../validations');
 
 describe('validations tests suites - isValid', () => {
-    test('should return true or false as the function is defined', () => {
-        const result = isValid();
-        expect(result).toBeDefined();
+    test('should return a invalid message if gamertag length is not be at least 8 characters', () => {
+        const result = isValid("s");
+        expect(result).toBe("Invalid - gamertag length must be at least 8 characters");
+    });
+    test('should return a valid message if gamertag length is at least 8 characters', () => {
+        const result = isValid("sfrefrefr");
+        expect(result).toBe("The gamerTag is valid");
     });
 });
--- src/services/validations.js
+++ src/services/validations.js
@@ -1,4 +1,7 @@
 // TODO: Refactor validation function to follow the defined validations rules
-const isValid = () => Math.random() >= 0.5;
+const isValid = (gamerTag) => {
+    if(gamerTag.length >= 8) return "The gamerTag is valid";
+    return "Invalid - gamertag length must be at least 8 characters";
+};
 
 exports.isValid = isValid;
